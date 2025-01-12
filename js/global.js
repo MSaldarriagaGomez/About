3ba6d8c7d3084fdcca8005f97cf137099490900d
natr-0 <ramosruiznatalia12@gmail.com>
2025-01-12 00:06:30 -0500

    Changes header, carousel and footer

--- js/global.js
+++ js/global.js
@@ -21,6 +21,7 @@
 /* 08 - Buttons, clicks, hovers */
 /* 09 - New */
 /* 10 - Shop page */
+/* 11 - About Us Natalia*/
 
 $(function() {
 
@@ -530,4 +531,19 @@ $(function() {
     });
 
 
+});
+
+    /*==============================*/
+    /* 11 - About Us Natalia */
+    /*==============================*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    const carousel = document.querySelector('#teamCarousel');
+    const items = carousel.querySelectorAll('.carousel-item');
+
+    carousel.addEventListener('slide.bs.carousel', (event) => {
+        items.forEach(item => item.querySelector('.card').style.transform = 'scale(1)');
+        const activeItem = items[event.to].querySelector('.card');
+        activeItem.style.transform = 'scale(1.2)';
+    });
 });
